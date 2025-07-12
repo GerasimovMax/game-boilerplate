2ef0936f77f13141cff8bf3368815c8c4ec16656
Maksim Gerasimov <gerasimovmaxx@gmail.com>
2025-07-12 17:00:16 +0300

    feat: add performance stats display in debug mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Canvas } from '@react-three/fiber'
+import { Stats } from '@react-three/drei'
 import { InputProvider } from '@/providers/input/InputProvider'
 import { KootaSystems } from '@/providers/KootaSystems'
 import { Physics } from '@react-three/rapier'
@@ -8,10 +9,11 @@ import { isDebug } from '@/shared/config'
 function App() {
   return (
     <Canvas shadows>
+      {isDebug && <Stats />}
       <InputProvider>
         <Physics debug={isDebug} timeStep="vary">
           <KootaSystems>
-              <DebugLevel />
+            <DebugLevel />
           </KootaSystems>
         </Physics>
       </InputProvider>
